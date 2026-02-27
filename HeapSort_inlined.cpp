900cbcb18e81724ccd0f159495c9484879a5642c
Qiwi <qiwi@qworker.qiwi.com>
2026-02-27 16:50:51 +0300

    add closest cpp representation of this algo. we use this to compare lines between vhd

---
+++ HeapSort_inlined.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+void printArr(int *arr, size_t size) {
+  std::cout << arr[0];
+  for (size_t i = 1; i < size; ++i) {
+    std::cout << " " << arr[i];
+  }
+  std::cout << "\n";
+}
+
+// void siftElement(int *arr, size_t size, size_t index) {
+//   while (true) {
+//     size_t l = 2 * index + 1;
+//     if (l >= size)
+//       break;
+//     size_t r = l + 1;
+//     size_t m = l;
+//     if (r < size && arr[r] < arr[l])
+//       m = r;
+//     if (arr[index] <= arr[m])
+//       break;
+//     std::swap(arr[index], arr[m]);
+//     index = m;
+//   }
+// }
+
+int *heapSort(int *arr, size_t size) {
+  for (int i = size / 2 - 1; i >= 0; --i) {
+    int index = i;
+    while (true) {
+      size_t l = 2 * index + 1;
+      if (l >= size)
+        break;
+      size_t r = l + 1;
+      size_t m = l;
+        if (r < size)
+        {
+            if (*(arr + r) < *(arr + l))
+            {
+             m = r;
+            }
+        }
+      if (*(arr + index) <= *(arr + m))
+        break;
+      std::swap(*(arr+index), *(arr+m));
+      index = m;
+    }
+    for (int i = size - 1; i >= 0; --i) {
+      std::swap(*(arr +i), *(arr));
+      int index = 0;
+      while (true) {
+        size_t l = 2 * index + 1;
+        if (l >= i)
+          break;
+        size_t r = l + 1;
+        size_t m = l;
+        if (r < i && *(arr+r) < *(arr+l))
+          m = r;
+        if (arr[index] <= arr[m])
+          break;
+        std::swap(arr[index], arr[m]);
+        index = m;
+      }
+      return arr;
+    }
+  }
+
+  int main() {
+    size_t size = 6;
+    int arr[6] = {24, 31, 15, 20, 52, 6};
+    printArr(arr, size);
+    int *sorted_arr = heapSort(arr, size);
+    printArr(sorted_arr, size);
+  }
