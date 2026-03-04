915e9e2af73bbe23d628e9b09d2f3f0e4cee0df8
Qiwi <qiwi@qworker.qiwi.com>
2026-03-04 16:30:10 +0300

    made swap number two
    Co-authored-by: RapidMindd <YaroslavT007@yandex.ru>

--- HeapSort_inlined.cpp
+++ HeapSort_inlined.cpp
@@ -33,43 +33,41 @@ int *heapSort(int *arr, size_t size) {
         break;
       size_t r = l + 1;
       size_t m = l;
-        if (r < size)
-        {
-            if (*(arr + r) < *(arr + l))
-            {
-             m = r;
-            }
+      if (r < size) {
+        if (*(arr + r) < *(arr + l)) {
+          m = r;
         }
+      }
       if (*(arr + index) <= *(arr + m))
         break;
-      std::swap(*(arr+index), *(arr+m));
+      std::swap(*(arr + index), *(arr + m));
       index = m;
     }
   }
-    for (int i = size - 1; i >= 0; --i) {
-      std::swap(*(arr +i), *(arr));
-      int index = 0;
-      while (true) {
-        size_t l = 2 * index + 1;
-        if (l >= i)
-          break;
-        size_t r = l + 1;
-        size_t m = l;
-        if (r < i && *(arr+r) < *(arr+l))
-          m = r;
-        if (arr[index] <= arr[m])
-          break;
-        std::swap(arr[index], arr[m]);
-        index = m;
-      }
+  for (int i = size - 1; i >= 0; --i) {
+    std::swap(*(arr + i), *(arr));
+    int index = 0;
+    while (true) {
+      size_t l = 2 * index + 1;
+      if (l >= i)
+        break;
+      size_t r = l + 1;
+      size_t m = l;
+      if (r < i && *(arr + r) < *(arr + l))
+        m = r;
+      if (arr[index] <= arr[m])
+        break;
+      std::swap(arr[index], arr[m]);
+      index = m;
     }
-      return arr;
   }
+  return arr;
+}
 
-  int main() {
-    size_t size = 6;
-    int arr[6] = {24, 31, 15, 20, 52, 6};
-    printArr(arr, size);
-    int *sorted_arr = heapSort(arr, size);
-    printArr(sorted_arr, size);
-  }
+int main() {
+  size_t size = 6;
+  int arr[6] = {24, 31, 15, 20, 52, 6};
+  printArr(arr, size);
+  int *sorted_arr = heapSort(arr, size);
+  printArr(sorted_arr, size);
+}
